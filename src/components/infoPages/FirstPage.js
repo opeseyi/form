c49e145c5edcc55f410ba288dc1ae101b1b2ac18
opeseyi <codinacct17@gmail.com>
2023-01-16 12:00:49 +0000

    ss

--- src/components/infoPages/FirstPage.js
+++ src/components/infoPages/FirstPage.js
@@ -3,12 +3,12 @@ import Summary from "./Summary";
 const FirstPage = () => {
   return (
     <section>
-      <Summary
+      {/* <Summary
         headText="Using the subway has never been easier"
         text1="Complete the sign up process and receive the QR code you can use to use the"
         text2="Subway anyway in the U.S."
-      />
-      <div></div>
+      /> */}
+      <div>First Page</div>
     </section>
   );
 };
